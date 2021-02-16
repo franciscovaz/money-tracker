c6a1e12716d38d5b38a5a33ef7d14fb3c7cda841
Francisco Vaz <ftomasvaz@gmail.com>
2021-02-16 12:08:52 +0000

    interface adjustments

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import { faPlusCircle, faMoneyCheckAlt, faCar, faShoppingCart, faUtensils, faGasPump } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +8,11 @@ import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 })
 export class HomeComponent implements OnInit {
   faPlusCircle = faPlusCircle;
+  faMoneyCheckAlt = faMoneyCheckAlt;
+  faCar = faCar;
+  faShoppingCart = faShoppingCart;
+  faUtensils = faUtensils;
+  faGasPump = faGasPump;
 
   constructor() { }
 
