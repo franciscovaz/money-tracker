809e8d9abbe7491c48b76676a0b316ea31c1df55
Francisco Vaz <ftomasvaz@gmail.com>
2021-02-17 20:11:40 +0000

    add table and some design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,8 @@ import { HeaderComponent } from './core/header/header.component';
 import { AngularMaterialModule } from './shared/angular-material.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HomeComponent } from './modules/home/home.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 
 @NgModule({
@@ -19,7 +22,10 @@ import { HomeComponent } from './modules/home/home.component';
     BrowserModule,
     AppRoutingModule,
     AngularMaterialModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    MatTableModule,
+    MatPaginatorModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,12 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { faPlusCircle, faMoneyCheckAlt, faCar, faShoppingCart, faUtensils, faGasPump } from '@fortawesome/free-solid-svg-icons';
+
+export interface PeriodicElement {
+  category: string;
+  description: string;
+  value: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { category: 'Vehicle', description: 'get new moto', value: '- 3000.5€' },
+  { category: 'Salary', description: 'One more', value: '+ 9999.5€' },
+  { category: 'Vehicle', description: 'New front tires', value: '- 120€' },
+  { category: 'Fuel', description: 'Car petrol', value: '- 40€' },
+  { category: 'Fuel', description: 'Moto Gasoline', value: '- 20€' },
+
+];
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
   faPlusCircle = faPlusCircle;
   faMoneyCheckAlt = faMoneyCheckAlt;
   faCar = faCar;
@@ -15,6 +34,15 @@ export class HomeComponent implements OnInit {
   faGasPump = faGasPump;
 
   constructor() { }
+
+  displayedColumns: string[] = ['category', 'description', 'value'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 
   ngOnInit(): void {
 
